dd06888bdb2705a3fa5bec42485b701c95867599
aadereiko <aliaksandr@comet.com>
2026-02-16 12:00:00 +0100

    fix bugs and improve the code;

--- apps/opik-backend/src/main/java/com/comet/opik/infrastructure/ServiceTogglesConfig.java
+++ apps/opik-backend/src/main/java/com/comet/opik/infrastructure/ServiceTogglesConfig.java
@@ -55,5 +55,5 @@ public class ServiceTogglesConfig {
     @JsonProperty
     @NotNull boolean agentConfigurationEnabled;
     @JsonProperty
-    @NotNull boolean onlineScoringDirectDataEnabled;
+    @NotNull boolean onlineEvaluationOptionalVariableMappingEnabled;
 }
